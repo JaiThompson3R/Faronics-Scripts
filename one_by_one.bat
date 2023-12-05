ba9204e1b1c94a2d0957257a9c697f3608275303
JaiThompson3R <152909269+JaiThompson3R@users.noreply.github.com>
2023-12-05 11:13:57 -0500

    Update one_by_one.bat

--- one_by_one.bat
+++ one_by_one.bat
@@ -1,18 +1,18 @@
 @echo off
 powershell start-process powershell -verb runas
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name ConfigureAutoMaticUpdates -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AutoInstallMinorUpdates -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name DetectionFrequencyEnabled -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name DetectionFrequency -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AlwaysAutoRebootAtScheduledTime -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AlwaysAutoRebootAtScheduledTimeMinutes -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name RescheduleWaitTimeEnabled -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AutomaticMaintenanceEnabled -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name ScheduledInstallDay -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name ScheduledInstallTime -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name ScheduledInstallEveryWeek -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AllowMUUpdateService -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AUOptions -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AutoInstallMinorUpdates -Value 0
-Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name NoAutoUpdate -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name ConfigureAutoMaticUpdates -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AutoInstallMinorUpdates -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name DetectionFrequencyEnabled -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name DetectionFrequency -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AlwaysAutoRebootAtScheduledTime -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AlwaysAutoRebootAtScheduledTimeMinutes -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name RescheduleWaitTimeEnabled -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AutomaticMaintenanceEnabled -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name ScheduledInstallDay -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name ScheduledInstallTime -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name ScheduledInstallEveryWeek -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AllowMUUpdateService -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AUOptions -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name AutoInstallMinorUpdates -Value 0
+powershell -command Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate -Name NoAutoUpdate -Value 0
 gpupdate /force
