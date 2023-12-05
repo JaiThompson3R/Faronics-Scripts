f19d0c9b422a6c2a30efe0d6f57e20609aa151b0
JaiThompson3R <152909269+JaiThompson3R@users.noreply.github.com>
2023-12-05 12:58:05 -0500

    Update one_by_one.bat

--- one_by_one.bat
+++ one_by_one.bat
@@ -1,19 +1,3 @@
 @echo off
-powershell start-process powershell -verb runas -ArgumentList
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name ConfigureAutoMaticUpdates -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name AutoInstallMinorUpdates -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name DetectionFrequencyEnabled -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name DetectionFrequency -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name AlwaysAutoRebootAtScheduledTime -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name AlwaysAutoRebootAtScheduledTimeMinutes -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name RescheduleWaitTimeEnabled -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name AutomaticMaintenanceEnabled -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name ScheduledInstallDay -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name ScheduledInstallTime -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name ScheduledInstallEveryWeek -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name AllowMUUpdateService -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name AUOptions -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name AutoInstallMinorUpdates -Value 0"
-powershell -command "Set-ItemProperty -Path HKLM:\Software\policies\microsoft\Windows\WindowsUpdate\AU -Name NoAutoUpdate -Value 0"
+reg delete HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU
 gpupdate /force
-exit
