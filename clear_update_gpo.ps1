36897d6830f22f2eb62cea25cf0a230ba0aef830
JaiThompson3R <152909269+JaiThompson3R@users.noreply.github.com>
2023-12-08 02:38:38 -0500

    Update clear_update_gpo.ps1

--- clear_update_gpo.ps1
+++ clear_update_gpo.ps1
@@ -1,3 +1,8 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
 if (Test-Path -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate") {
     Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Recurse
 }
